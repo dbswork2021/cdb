018a6a5baaf2b3a6d566ac241fbaa87bfb9c6bc4
Falcon <leonsout@outlook.com>
2022-04-30 17:51:12 +0800

    购买BUG

--- routes/web/product.js
+++ routes/web/product.js
@@ -15,7 +15,7 @@ productRoutes.post('/buy', async (req, res) => {
 	const currentUser = await userSchema.findById(req.user.id)
 	assert(currentUser.blance >= req.body.price, 400, res.__('blance_not_enough'))
 	await productSchema.findOneAndUpdate(req.body._id, {$inc: {stock: -1}}) // 库存减一
-  const newUserInfo =	await userSchema.findOneAndUpdate(currentUser._id, {$inc: {blance: -req.body.price}}, {new: true}) // 扣除余额
+  const newUserInfo =	await userSchema.findByIdAndUpdate(currentUser._id, {$inc: {blance: -req.body.price}}, {new: true}) // 扣除余额
 	await recordSchema.create({user: newUserInfo._id, amount: req.body.price, blance: newUserInfo.blance, description: "购买" + req.body.name}) // 财务明细
 	await tridingSchema.create({user: newUserInfo._id, product: req.body._id, income: req.body.income, price: req.body.price}) // 交易记录
 	res.send({message: res.__('triding_done')})
