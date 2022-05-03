c0db54bdb72323c5a127bdf139dac70747672d79
Falcon <leonsout@outlook.com>
2022-05-03 21:20:57 +0800

    添加支付跳转地址

--- routes/web/order.js
+++ routes/web/order.js
@@ -30,8 +30,8 @@ orderRoutes.post('/recharge', async (req, res) => {
     setResult.sets.get('notifyUrl') +
     '&order_date=' +
     date +
-			'page_url=' +
-			setResult.sets.get('jumpUrl')
+    'page_url=' +
+    setResult.sets.get('jumpUrl') +
     '&pay_type=' +
     setResult.sets.get('payType') +
     '&trade_amount=' +
@@ -47,7 +47,7 @@ orderRoutes.post('/recharge', async (req, res) => {
     mch_order_no: mchOrderId,
     notify_url: setResult.sets.get('notifyUrl'),
     order_date: date,
-		page_url: setResult.sets.get('jumpUrl'),
+    page_url: setResult.sets.get('jumpUrl'),
     pay_type: setResult.sets.get('payType'),
     trade_amount: orderInfo.amount,
     version: '1.0',
